3352f26c242621bc115f0b3bda647fc7081b232d
Quincy Schurr <qschurr@smu.edu>
2015-12-12 16:08:07 -0600

    we have song results!!!!!!!!

--- iTunes/app/src/main/java/com/example/jake/itunes/MusicResultsList.java
+++ iTunes/app/src/main/java/com/example/jake/itunes/MusicResultsList.java
@@ -3,12 +3,14 @@ package com.example.jake.itunes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -23,7 +25,7 @@ public class MusicResultsList extends AppCompatActivity{
     private ImageButton heart;
     private Toolbar toolbar;
     private ListView lSongs;
-
+    private SongAdapter songAdapter;
     public static final String song_Detail = "event";
     private static final String TAG = "MusicResultsList";
 
@@ -31,10 +33,15 @@ public class MusicResultsList extends AppCompatActivity{
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-
+        ArrayList<Song> songs = (ArrayList<Song>)intent.getSerializableExtra("SendIntent");
         setContentView(R.layout.song_results_list);
 
         lSongs = (ListView) findViewById(R.id.listResults);
+        songAdapter = new SongAdapter(this, songs);
+        lSongs.setAdapter(songAdapter);
+
 
     }
+
+
 }
